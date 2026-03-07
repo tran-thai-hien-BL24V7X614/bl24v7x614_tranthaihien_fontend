37e0aa03ec4b9138787433df7c60df8c6af6dfdc
thai hien <tranthaihien2021@gmail.com>
2026-03-07 17:43:53 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
-<script>
-export default {
-}
-</script>
 <template>
-<h1>Hello, Vue.js!</h1>
+  <div id="app">
+    <AppHeader />
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
+
+<script>
+import ContactCard from "@/components/ContactCard.vue";
+import InputSearch from "@/components/InputSearch.vue";
+import ContactList from "@/components/ContactList.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactCard,
+    InputSearch,
+    ContactList,
+  },
+
+  data() {
+    return {
+      contacts: [],
+      activeIndex: -1,
+      searchText: "",
+    };
+  },
+
+  methods: {
+    async retrieveContacts() {
+      try {
+        this.contacts = await ContactService.getAll();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
 <style>
 .page {
-max-width: 400px;
-margin: auto;
+  max-width: 400px;
+  margin: auto;
 }
 </style>
